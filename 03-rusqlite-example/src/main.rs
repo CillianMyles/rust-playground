e959da82e59bcb9f98c2f5dd8649c4a1d52235ba
Cillian Myles <myles.cillian@gmail.com>
2024-05-04 22:04:53 +0100

    feat: 03: rusqlite example: add + populate stats table

--- 03-rusqlite-example/src/main.rs
+++ 03-rusqlite-example/src/main.rs
@@ -6,14 +6,36 @@ fn main() -> Result<()> {
     conn.execute(
         "CREATE TABLE IF NOT EXISTS languages (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE
+            name TEXT NOT NULL UNIQUE,
+            compiled BOOLEAN NOT NULL
+        )",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS stats (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            language_id INTEGER NOT NULL,
+            ease_of_use REAL NOT NULL,
+            performance REAL NOT NULL,
+            FOREIGN KEY (language_id) REFERENCES languages(id)
         )",
         [],
     )?;
 
     conn.execute(
         "INSERT OR IGNORE INTO languages (name) VALUES (?1), (?2), (?3), (?4), (?5)",
-        &["Python", "Java", "Kotlin", "Dart", "Rust"],
+        ["Python", "Java", "Kotlin", "Dart", "Rust"],
+    )?;
+
+    conn.execute(
+        "INSERT OR IGNORE INTO stats (language_id, ease_of_use, performance) VALUES
+            ((SELECT id FROM languages WHERE name = 'Python'), 78.0, 33.0),
+            ((SELECT id FROM languages WHERE name = 'Java'), 59.0, 51.0),
+            ((SELECT id FROM languages WHERE name = 'Kotlin'), 63.0, 49.0),
+            ((SELECT id FROM languages WHERE name = 'Dart'), 81.0, 78.0),
+            ((SELECT id FROM languages WHERE name = 'Rust'), 47.0, 93.0)",
+        [],
     )?;
 
     Ok(())
