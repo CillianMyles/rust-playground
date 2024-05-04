5322ce91ed45ef37c0c5d7861188441434e73bef
Cillian Myles <myles.cillian@gmail.com>
2024-05-04 23:04:22 +0100

    feat: 03: rusqlite example: query, compute, & rank by stats for languages

--- 03-rusqlite-example/src/main.rs
+++ 03-rusqlite-example/src/main.rs
@@ -7,36 +7,39 @@ fn main() -> Result<()> {
         "CREATE TABLE IF NOT EXISTS languages (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL UNIQUE,
-            compiled BOOLEAN NOT NULL
-        )",
-        [],
-    )?;
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS stats (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            language_id INTEGER NOT NULL,
             ease_of_use REAL NOT NULL,
-            performance REAL NOT NULL,
-            FOREIGN KEY (language_id) REFERENCES languages(id)
+            performance REAL NOT NULL
         )",
         [],
     )?;
 
     conn.execute(
-        "INSERT OR IGNORE INTO languages (name) VALUES (?1), (?2), (?3), (?4), (?5)",
-        ["Python", "Java", "Kotlin", "Dart", "Rust"],
+        "INSERT OR IGNORE INTO languages (name, ease_of_use, performance) VALUES (?1, ?2, ?3), (?4, ?5, ?6), (?7, ?8, ?9), (?10, ?11, ?12), (?13, ?14, ?15)",
+        ["Python", "78.0", "33.0", "Java", "59.0", "51.0", "Kotlin", "63.0", "49.0", "Dart", "81.0", "78.0", "Rust", "47.0", "93.0"],
     )?;
 
-    conn.execute(
-        "INSERT OR IGNORE INTO stats (language_id, ease_of_use, performance) VALUES
-            ((SELECT id FROM languages WHERE name = 'Python'), 78.0, 33.0),
-            ((SELECT id FROM languages WHERE name = 'Java'), 59.0, 51.0),
-            ((SELECT id FROM languages WHERE name = 'Kotlin'), 63.0, 49.0),
-            ((SELECT id FROM languages WHERE name = 'Dart'), 81.0, 78.0),
-            ((SELECT id FROM languages WHERE name = 'Rust'), 47.0, 93.0)",
-        [],
+    let mut stmt = conn.prepare(
+        "SELECT name, ease_of_use, performance, (ease_of_use * performance) as score
+        FROM languages
+        ORDER BY score DESC",
     )?;
+
+    let rows = stmt.query_map([], |row| {
+        Ok((
+            row.get::<_, String>(0)?,
+            row.get::<_, f64>(1)?,
+            row.get::<_, f64>(2)?,
+            row.get::<_, f64>(3)?,
+        ))
+    })?;
+
+    for row in rows {
+        let (name, ease_of_use, performance, score) = row.unwrap();
+        println!(
+            "{} ( ease_of_use: {}, performance: {}, score: {} )",
+            name, ease_of_use, performance, score,
+        );
+    }
 
     Ok(())
 }
