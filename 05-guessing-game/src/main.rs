950f0995362389ac5777422e6237d1380161691c
Cillian Myles <myles.cillian@gmail.com>
2024-06-02 14:41:35 +0100

    feat: 05: guessing game: add game loop

--- 05-guessing-game/src/main.rs
+++ 05-guessing-game/src/main.rs
@@ -6,24 +6,32 @@ fn main() {
     let secret_number: u32 = rand::thread_rng().gen_range(0..=100);
     println!("Secret number: {secret_number}");
 
-    println!("Guess a number: ");
+    loop {
+        println!("Guess a number: ");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    let guess: u32 = match guess.trim().parse() {
-        Ok(value) => {
-            println!("You guessed: {guess}");
-            value
+        let guess: u32 = match guess.trim().parse() {
+            Ok(value) => {
+                println!("You guessed: {guess}");
+                value
+            }
+            Err(_) => {
+                println!("Please input a number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
-        Err(_) => panic!("Please input a number!"),
-    };
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too low!"),
-        Ordering::Greater => println!("Too high!"),
-        Ordering::Equal => println!("You win!"),
     }
 }
