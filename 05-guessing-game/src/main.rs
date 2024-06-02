4fafbcfd7e51f56dc2db91806b5d5e4a75bd1fe6
Cillian Myles <myles.cillian@gmail.com>
2024-06-02 14:10:05 +0100

    feat: 05: compare guess to randomly generated secret number

--- 05-guessing-game/src/main.rs
+++ 05-guessing-game/src/main.rs
@@ -1,11 +1,29 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello...");
+    let secret_number: u32 = rand::thread_rng().gen_range(0..=100);
+    println!("Secret number: {secret_number}");
+
     println!("Guess a number: ");
 
     let mut guess = String::new();
-    std::io::stdin()
+    io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line!");
 
-    println!("You guessed: {guess}");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(value) => {
+            println!("You guessed: {guess}");
+            value
+        }
+        Err(_) => panic!("Please input a number!"),
+    };
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low!"),
+        Ordering::Greater => println!("Too high!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
